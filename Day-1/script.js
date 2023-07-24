962920d58e46af198696e3f29a1f7f5f94147fe6
Bala Akash <balaakash.career@gmail.com>
2023-07-24 06:40:49 -0700

    Update script.js
    exercise

---
+++ Day-1/script.js
@@ -0,0 +1,71 @@
+// function countSheeps(arrayOfSheep) {
+//   // TODO May the force be with you
+//   for (let i = 0; i < arrayOfSheep.length; i++) {
+//     if (arrayOfSheep[i] === true) {
+//       count++;
+//       //   console.log(count);
+//     }
+//   }
+//   console.log(count);
+// }
+// let count = 0;
+// let arrayOfSheep = [
+//   true,
+//   true,
+//   true,
+//   false,
+//   true,
+//   true,
+//   true,
+//   true,
+//   true,
+//   false,
+//   true,
+//   false,
+//   true,
+//   false,
+//   false,
+//   true,
+//   true,
+//   true,
+//   true,
+//   true,
+//   false,
+//   false,
+//   true,
+//   true,
+// ];
+// countSheeps(arrayOfSheep);
+
+function countSheep(sheepArray) {
+  return sheepArray.filter(Boolean).length;
+}
+const sheepArray = [
+  true,
+  true,
+  true,
+  false,
+  true,
+  true,
+  true,
+  true,
+  true,
+  false,
+  true,
+  false,
+  true,
+  false,
+  false,
+  true,
+  true,
+  true,
+  true,
+  true,
+  false,
+  false,
+  true,
+  true,
+];
+
+const sheepCount = countSheep(sheepArray);
+console.log("Number of sheep present:", sheepCount);
